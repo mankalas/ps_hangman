1089917f32fb00c1a167addab1ab78d5d6123ff9
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-02 13:58:01 +1200

    Fix the rspec deprecation warning.

--- hangman_spec.rb
+++ hangman_spec.rb
@@ -9,48 +9,48 @@ describe Hangman do
   end
 
   it "should return only underscores when no guess has been made" do
-    @hangman.show_progress.should == "_" * TEST_WORD.length
+    expect(@hangman.show_progress).to eq "_" * TEST_WORD.length
   end
 
   it "should show a rightly guessed letter" do
     @hangman.guess("e")
-    @hangman.show_progress.should == "____e"
+    expect(@hangman.show_progress).to eq "____e"
   end
 
   it "should not show a wrongly guessed letter" do
     @hangman.guess("a")
-    @hangman.show_progress.should == "_" * TEST_WORD.length
+    expect(@hangman.show_progress).to eq "_" * TEST_WORD.length
   end
 
   it "should not show a badly cased letter if hangman is case sensitive" do
     @hangman.case_sensitive = true
     @hangman.guess("h")
-    @hangman.show_progress.should == "_" * TEST_WORD.length
+    expect(@hangman.show_progress).to eq "_" * TEST_WORD.length
     @hangman.guess("H")
-    @hangman.show_progress.should == "H____"
+    expect(@hangman.show_progress).to eq "H____"
   end
 
   it "should show a badly cased letter if hangman is case sensitive" do
     @hangman.case_sensitive = false
     @hangman.guess("r")
-    @hangman.show_progress.should == "__R__"
+    expect(@hangman.show_progress).to eq "__R__"
     @hangman.guess("S")
-    @hangman.show_progress.should == "__RS_"
+    expect(@hangman.show_progress).to eq "__RS_"
   end
 
   it "should end with a win if all letters have been guessed" do
-    @hangman.game_over?.should == false
+    expect(@hangman.game_over?).to eq false
     TEST_WORD.each_char { |c| @hangman.guess(c) }
-    @hangman.game_over?.should == true
-    @hangman.word_guessed?.should == true
+    expect(@hangman.game_over?).to eq true
+    expect(@hangman.word_guessed?).to eq true
   end
 
   it "should end with a lose if the word has not been guessed after a
     number of tries" do
-    @hangman.game_over?.should == false
+    expect(@hangman.game_over?).to eq false
     10.times { @hangman.guess('x') }
-    @hangman.game_over?.should == true
-    @hangman.word_guessed?.should == false
+    expect(@hangman.game_over?).to eq true
+    expect(@hangman.word_guessed?).to eq false
   end
 
 end
