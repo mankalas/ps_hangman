41a61f250573a6392dd23989f9f8ab25ac70a464
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-02 17:11:55 +1200

    If present, the game can retrieve words from the system's dictionary.

--- hangman.rb
+++ hangman.rb
@@ -1,14 +1,15 @@
 module Hangman
   MAX_GUESSES = 5
   WORDS = %w(hello world guinness food sea oxymoron)
+  UNIX_WORDS_PATH = '/usr/share/dict/words'
 
   class Engine
     attr_accessor :case_sensitive
     attr_reader :number_of_wrong_guesses
     attr_reader :word
 
-    def initialize(word = WORDS.sample)
-      @word = word
+    def initialize(word = nil)
+      @word = (word ? word : get_dictionary.sample)
       @number_of_wrong_guesses ||= 0
       @guessed_letters = ""
       @case_sensitive = true
@@ -44,7 +45,16 @@ module Hangman
     def no_more_guesses?
       @number_of_wrong_guesses >= MAX_GUESSES
     end
+
+    private
+
+    def get_dictionary
+      begin
+        File.read(UNIX_WORDS_PATH).split
+      rescue Errno::ENOENT
+        puts "No dictionary found. Falling back to simple word list."
+        WORDS
+      end
+    end
   end
-
-
 end
