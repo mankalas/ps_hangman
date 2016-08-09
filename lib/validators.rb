a87bce20ae3b202855d8e663efb9d15e7bfd91c2
Vincent Boucheny <vincent.boucheny@powershop.co.nz>
2016-08-10 10:49:00 +1200

    Tidying validator code & tests.

--- lib/validators.rb
+++ lib/validators.rb
@@ -1,30 +1,90 @@
+require 'set'
+
 module Hangman
   class Validator
-    def initialize(word)
-      @word = word
-      @guessed_letters = ""
+    def initialize
+      @guessed_letters = Set.new
     end
-  end
 
-  class CaseSensitiveValidator < Validator
-    def validate(letter)
+    def register_letter(letter)
       @guessed_letters << letter
-      @word.include?(letter)
     end
 
     def letter_guessed?(letter)
       @guessed_letters.include?(letter)
     end
+
+    def progress(word)
+      word.each_char.collect do |char|
+        [char, letter_guessed?(char)]
+      end.to_h
+    end
+
+    def word_guessed?(word)
+      word.each_char.all?(&method(:letter_guessed?))
+    end
+
+    def valid_input?(letter)
+      letter and letter != ''
+    end
+  end
+
+  class CaseSensitiveValidator < Validator
+    def validate(letter, word)
+      if valid_input?(letter)
+        register_letter(letter)
+        word.include?(letter)
+      end
+    end
   end
 
   class CaseInsensitiveValidator < Validator
-    def validate(letter)
-      @guessed_letters << letter.downcase
-      @word.downcase.include?(letter.downcase)
+    def validate(letter, word)
+      if valid_input?(letter)
+        register_letter(letter)
+        word.downcase.include?(letter.downcase)
+      end
     end
 
     def letter_guessed?(letter)
-      @guessed_letters.include?(letter.downcase)
+      super(letter.downcase)
+    end
+
+    def register_letter(letter)
+      super(letter.downcase)
     end
   end
+
+  # class FuzzyValidator < Validator
+  #   def initialize(word)
+  #     super(word)
+  #     @next = CaseSensitiveValidator.new(word)
+  #   end
+
+  #   def validate(letter)
+  #     if valid_input?(letter)
+  #       (-1..1).each do |index|
+  #         current_letter = (letter.ord + index).chr
+  #         @next.validate(current_letter)
+  #       end.any?
+  #     end
+  #   end
+  # end
+
+  # class OrderedValidator < Validator
+  #   def initialize(word)
+  #     super(word)
+  #     @rank = 0
+  #   end
+
+  #   def validate(letter)
+  #     full_word = @word
+  #     @word = full_word[@rank]
+  #     if @next.validate(letter)
+  #       @rank += 1
+  #       @guessed_letters = ""
+  #     end
+  #     @word = full_word
+  #   end
+  # end
 end
--- spec/validators_spec.rb
+++ spec/validators_spec.rb
@@ -1,37 +1,103 @@
-require 'views'
+require 'validators'
 
-describe Hangman::CaseSensitiveValidator do
+describe Hangman::Validator do
 
-  let(:validator) { Hangman::CaseSensitiveValidator.new(TEST_WORD) }
+  let(:test_word) { "HoRSe" }
 
-  it "should validate only right cased letters" do
-    expect(validator.validate('H')).to be true
-    expect(validator.letter_guessed?('H')).to be true
-    expect(validator.letter_guessed?('h')).to be false
-    expect(validator.validate('o')).to be true
-    expect(validator.letter_guessed?('o')).to be true
-    expect(validator.letter_guessed?('h')).to be false
-    expect(validator.letter_guessed?('O')).to be false
-    expect(validator.validate('h')).to be false
-    expect(validator.letter_guessed?('h')).to be true
-    expect(validator.validate('O')).to be false
-    expect(validator.letter_guessed?('h')).to be true
-    expect(validator.validate('x')).to be false
+  shared_examples "a validator" do
+    it "invalidates bad input" do
+      expect(validator.validate('1', test_word)).to be_falsey
+      expect(validator.validate('%', test_word)).to be_falsey
+      expect(validator.validate('aa', test_word)).to be_falsey
+      expect(validator.validate('', test_word)).to be_falsey
+      expect(validator.validate(nil, test_word)).to be_falsey
+    end
+
+    context "when a good letter has been guessed" do
+      let(:expected_progress) do
+        test_word.each_char.collect{ |char| [char, char == 'H'] }.to_h
+      end
+
+      it "updates progress" do
+        validator.validate('H', test_word)
+        expect(validator.progress(test_word)).to match expected_progress
+      end
+
+      it "does not update progress twice" do
+        validator.validate('H', test_word)
+        expect{ validator.validate('H', test_word) }.not_to change { validator.progress(test_word) }
+      end
+    end
+
+    context "when a bad letter has been guesses" do
+      it "does not update progress" do
+        expect{ validator.validate('x', test_word) }.not_to change { validator.progress(test_word) }
+      end
+    end
   end
-end
 
-describe Hangman::CaseInsensitiveValidator do
-
-  let(:validator) { Hangman::CaseInsensitiveValidator.new(TEST_WORD) }
-
-  it "should validate letters no matter the case" do
-    expect(validator.validate('H')).to be true
-    expect(validator.letter_guessed?('H')).to be true
-    expect(validator.letter_guessed?('h')).to be true
-    expect(validator.validate('o')).to be true
-    expect(validator.letter_guessed?('o')).to be true
-    expect(validator.letter_guessed?('O')).to be true
-    expect(validator.validate('r')).to be true
-    expect(validator.validate('x')).to be false
+  shared_examples "a strict validator" do
+    it "invalidates wrong letters, no matter the case" do
+      expect(validator.validate('x', test_word)).to be_falsey
+      expect(validator.validate('X', test_word)).to be_falsey
+    end
   end
+
+  describe Hangman::CaseSensitiveValidator do
+    let(:validator) { Hangman::CaseSensitiveValidator.new }
+
+    it_behaves_like "a validator"
+    it_behaves_like "a strict validator"
+
+    it "validates upper cased letters" do
+      expect(validator.letter_guessed?('H')).to be_falsey
+      expect(validator.validate('H', test_word)).to be_truthy
+      expect(validator.letter_guessed?('H')).to be_truthy
+    end
+
+    it "validates lower cases letters" do
+      expect(validator.letter_guessed?('o')).to be_falsey
+      expect(validator.validate('o', test_word)).to be_truthy
+      expect(validator.letter_guessed?('o')).to be_truthy
+    end
+
+    it "invalidates wrongly cased letters" do
+      expect(validator.validate('r', test_word)).to be_falsey
+      expect(validator.letter_guessed?('r')).to be_truthy
+      expect(validator.letter_guessed?('R')).to be_falsey
+
+      expect(validator.validate('E', test_word)).to be_falsey
+      expect(validator.letter_guessed?('E')).to be_truthy
+    end
+  end
+
+  describe Hangman::CaseInsensitiveValidator do
+    let(:validator) { Hangman::CaseInsensitiveValidator.new }
+
+    it_behaves_like "a validator"
+    it_behaves_like "a strict validator"
+
+    it "validates letters no matter the case" do
+      expect(validator.validate('H', test_word)).to be_truthy
+      expect(validator.letter_guessed?('H')).to be_truthy
+      expect(validator.letter_guessed?('h')).to be_truthy
+
+      expect(validator.validate('o', test_word)).to be_truthy
+      expect(validator.letter_guessed?('o')).to be_truthy
+      expect(validator.letter_guessed?('O')).to be_truthy
+    end
+  end
+
+  # Neutralized... not relevant for now.
+
+  # describe Hangman::FuzzyValidator do
+  #   let (:validator) { Hangman::FuzzyValidator.new(test_word) }
+
+  #   it_behaves_like "a validator"
+
+  #   it "validates the letter before, and after the one given" do
+  #     expect(validator.validate('I', test_word)).to be_truthy
+  #     expect(validator.letter_guessed?('J', test_word)).to be_truthy
+  #   end
+  # end
 end
